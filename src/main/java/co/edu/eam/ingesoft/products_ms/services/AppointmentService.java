d695e6128b23924d74a6f3c80f803c4956766513
m9n0lo <xthernux@gmail.com>
2019-09-19 23:31:49 -0500

    Crear_cita [BACK crear cita psicologia]

--- src/main/java/co/edu/eam/ingesoft/products_ms/services/AppointmentService.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/services/AppointmentService.java
@@ -1,12 +1,6 @@
 package co.edu.eam.ingesoft.products_ms.services;
 
-import javax.persistence.EntityExistsException;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import co.edu.eam.ingesoft.products_ms.model.Cita;
-import co.edu.eam.ingesoft.products_ms.repositories.AppointmentRepository;
 
 /**
  * Service to perform bussines operations over Appointment entity.

3d586c62e8c75451623d6115c75f62159f71d43b
m9n0lo <xthernux@gmail.com>
2019-09-21 12:15:44 -0500

    migracion_cita

--- src/main/resources/db/migration/V1.2__cita.sql
+++ src/main/resources/db/migration/V1.2__cita.sql
@@ -1,9 +1,9 @@
 CREATE TABLE public.cita
 (
-    idCita       bigint PRIMARY KEY,
-    fechaHora     DATE NOT NULL,
-    psicologoCedula    VARCHAR(30) NOT NULL,
-    estudianteId     bigint NOT NULL,
+    id_cita       bigint PRIMARY KEY,
+    fecha_hora     DATE NOT NULL,
+    psicologo_cedula    VARCHAR(30) NOT NULL,
+    estudiante_id     VARCHAR(30) NOT NULL,
     estado        VARCHAR(45) NOT NULL,
     titulo        VARCHAR(100),
     descripcion    VARCHAR(500)

954676182acf53d8238e5c72954992a85715db44
m9n0lo <xthernux@gmail.com>
2019-09-21 12:21:53 -0500

    migracion_cita

--- src/test/java/co/edu/eam/ingesoft/productms/test/controllers/AppointmentControllerTest.java
+++ src/test/java/co/edu/eam/ingesoft/productms/test/controllers/AppointmentControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.assertj.core.util.Lists;
@@ -29,6 +30,11 @@ import co.edu.eam.ingesoft.products_ms.routes.Router;
 @ContextConfiguration(classes = { Application.class })
 public class AppointmentControllerTest {
 
+  String pattern = "yyyy-MM-dd HH:mm:ss.S";
+  SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+  String date = "2015-06-05 00:00:00.0";
+
   @Autowired
   private MockMvc mockMvc;
 
@@ -44,13 +50,13 @@ public class AppointmentControllerTest {
 
   @Test
   public void save() throws Exception {
-    String content = "{\"descripcion\":\"camilo\",\"titulo\":\"prueba\",\"estado\":\"activo\",\"estudianteId\":1,\"psicologoCedula\":\"1\",\"fechaHora\":\"04/05/2015\",\"idCita\":1 }";
+    String content = "{\"descripcion\":\"camilo\",\"titulo\":\"prueba\",\"estado\":\"activo\",\"estudianteId\":\"1\",\"psicologoCedula\":\"1\",\"fechaHora\":\"2015-06-05T05:00\",\"idCita\":1 }";
     mockMvc.perform(post(SAVE).content(content).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     
     Cita dateToAssert = appointmentRespository.findById(new Integer(1)).get();
-    assertEquals("09/04/05", dateToAssert.getFechaHora());
+    assertEquals(date, dateToAssert.getFechaHora());
     assertEquals("1", dateToAssert.getPsicologoCedula());
-    assertEquals(new Integer(1), dateToAssert.getEstudianteId());
+    assertEquals("1", dateToAssert.getEstudianteId());
     assertEquals("activo", dateToAssert.getEstado());
     assertEquals("entrevista practica", dateToAssert.getTitulo());
     assertEquals("temas basicos de la persona", dateToAssert.getDescripcion());
@@ -58,8 +64,8 @@ public class AppointmentControllerTest {
 
   @Test
   public void saveAlreadyExists() throws Exception {
-    appointmentRespository.saveAll(Lists.list(new Cita(1,new Date(06/05/2018),"1",1,"activo","prueba","camilo"), new Cita(2,new Date(06/07/2016),"2",2,"cerrado","entrevista profundizada","temas del trabajo a realizar")));
-    String content = "{\"descripcion\":\"camilo\",\"titulo\":\"prueba\",\"estado\":\"activo\",\"estudianteId\":1,\"psicologoCedula\":\"1\",\"fechaHora\":\"04/05/2015\",\"idCita\":1 }";
+    appointmentRespository.saveAll(Lists.list(new Cita(1,new Date(06/05/2018),"1","1","activo","prueba","camilo"), new Cita(2,new Date(06/07/2016),"2","2","cerrado","entrevista profundizada","temas del trabajo a realizar")));
+    String content = "{\"descripcion\":\"camilo\",\"titulo\":\"prueba\",\"estado\":\"activo\",\"estudianteId\":\"1\",\"psicologoCedula\":\"1\",\"fechaHora\":\"2015-04-05T05:00\",\"idCita\":1 }";
 
     mockMvc.perform(post(SAVE).content(content).contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(409));
   }

89ca5312dd2dddf44f2225bc4b00dcf0657931e5
conrado94 <48019268+conrado94@users.noreply.github.com>
2019-10-26 08:54:14 -0500

    [resolves # 3]modify_appointmen

--- src/main/java/co/edu/eam/ingesoft/products_ms/controllers/AppointmentController.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/controllers/AppointmentController.java
@@ -1,20 +1,19 @@
 package co.edu.eam.ingesoft.products_ms.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.eam.ingesoft.products_ms.model.Cita;
-import co.edu.eam.ingesoft.products_ms.model.Person;
 import co.edu.eam.ingesoft.products_ms.routes.Router;
 import co.edu.eam.ingesoft.products_ms.services.AppointmentService;
 
@@ -27,51 +26,51 @@ import co.edu.eam.ingesoft.products_ms.services.AppointmentService;
 @RestController
 @RequestMapping(Router.APPOINTMENT_PATH)
 public class AppointmentController {
-	/**
-	 * date service.
-	 */
-	@Autowired
-	private AppointmentService appointmentService;
+  /**
+* date service.
+   */
+  @Autowired
+  private AppointmentService appointmentService;
 
-	/**
-	 * create a date operation.
-	 *
-	 * @param cita date to create
-	 * @return date created
-	 */
-	@PostMapping(value = Router.CREATE_APPOINTMENT)
-	public Cita create(@RequestBody Cita cita) {
-		return appointmentService.create(cita);
-	}
+  /**
+   * create a date operation.
+   *
+   * @param cita date to create
+   * @return date created
+   */
+  @PostMapping(value = Router.CREATE_APPOINTMENT)
+  public Cita create(@RequestBody Cita cita) {
+    return appointmentService.create(cita);
+  }
 
-	/**
-	 * find a cita by psicologoCedula and estado.
-	 *
-	 * @param psicologoCedula psicologoCedula cita to find
-	 * @param estado          estado cita to find
-	 * @return list of person with a psicologoCedula and estado
-	 */
-	@GetMapping(value = Router.FIND_BY_PSICOLOGOCEDULA_ESTADO)
-	public ResponseEntity<List<Cita>> findByPsicologoCedulaAndEstadoOrderByFechaHora(
-			@RequestParam String psicologoCedula, @RequestParam String estado) {
-		List<Cita> cita = appointmentService.findByPsicologoCedulaOrEstadoOrderByFechaHora(psicologoCedula, estado);
+  /**
+   * find a cita by psicologoCedula and estado.
+   *
+   * @param psicologoCedula psicologoCedula cita to find
+   * @param estado          estado cita to find
+   * @return list of person with a psicologoCedula and estado
+   */
+  @GetMapping(value = Router.FIND_BY_PSICOLOGOCEDULA_ESTADO)
+  public ResponseEntity<List<Cita>> findByPsicologoCedulaAndEstadoOrderByFechaHora(@RequestParam String psicologoCedula,
+      @RequestParam String estado) {
+    List<Cita> cita = appointmentService.findByPsicologoCedulaOrEstadoOrderByFechaHora(psicologoCedula, estado);
 
-		if (cita.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
+    if (cita.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
-		return new ResponseEntity<>(cita, HttpStatus.OK);
-	}
+    return new ResponseEntity<>(cita, HttpStatus.OK);
+  }
 
-	/**
-	 * Edit a appointmen.
-	 *
-	 * @param appointmen person to edit
-	 * @return appointmen edited
-	 */
-	@PutMapping(value = Router.EDIT_APPOINTMEN)
-	public Cita edit(@RequestBody Cita appointmen) {
-		return appointmentService.update(appointmen);
-	}
+  /**
+   * Edit a appointmen.
+   *
+   * @param appointmen person to edit
+   * @return appointmen edited
+   */
+  @PutMapping(value = Router.EDIT_APPOINTMEN)
+  public Cita edit(@RequestBody Cita appointmen) {
+    return appointmentService.update(appointmen);
+  }
 
 }
